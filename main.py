e3c43559641eec1bf360fbb7a628fed46261436b
h7-v <h7-v@proton.me>
2023-09-07 01:03:11 +0100

    Add bluetooth connect and disconnect functionality while running. Detect current playing or paused song and update GUI without requiring a song change

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+# Get information about BT device player0 or fd0 using
+# dbus-monitor --system "type='signal',sender='org.bluez'"
+# Occasionally the object path /player0 is not available and /fd0 is used
+# instead. The player does not function properly if this is the case.
+
+# TODO: Check for the fd0 case and reset either the dbus connection
+# of the bluetooth service.
+
 import subprocess
+import time
 from PyQt5.QtCore import Qt, QTimer, QRect
 from PyQt5.QtGui import QPainter, QFontMetrics
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QVBoxLayout
@@ -10,30 +19,80 @@ from gi.repository import GLib
 from functools import partial
 
 
+def get_connected_bluetooth_mac(bus_object, window):
+    bluez_service = bus_object.get('org.bluez', '/')
+    managed_objects = bluez_service.GetManagedObjects()
+
+    # Loop through all managed objects to find connected device
+    for path, interfaces in managed_objects.items():
+        if 'org.bluez.Device1' in interfaces:
+            device = bus_object.get('org.bluez', path)
+
+            device.PropertiesChanged.connect(partial(
+                on_device_property_changed, window))
+
+            device_properties = managed_objects[path]['org.bluez.Device1']
+            if 'Connected' in device_properties and \
+               device_properties['Connected']:
+                return path.split('/')[-1]  # Extract MAC address from the path
+
+    return None  # Return None if no connected device is found
+
+
+# Used to handle connections and disconnections of Bluetooth devices.
+def on_device_property_changed(window, *args, **kwargs):
+    # print("All args:", args)  # Debugging line
+    # print("All kwargs:", kwargs) # Debugging line
+
+    interface_name = args[0]
+    properties = args[1]
+
+    # print(f"Interface: {interface_name}")  # Debugging line
+    # print(f"Properties: {properties}")  # Debugging line
+
+    try:
+        if interface_name == 'org.bluez.Device1':
+            if 'Connected' in properties:
+                is_connected = properties['Connected']
+                if is_connected:
+                    print("A Bluetooth device has connected.")
+                    # You can add your logic here to handle the new connection
+                    window.handle_new_connection()
+                else:
+                    print("A Bluetooth device has disconnected.")
+                    # You can add your logic here to handle the disconnection
+                    window.handle_disconnection()
+    except Exception as e:
+        print(f"An error occurred in on_device_property_changed: {e}")
+
+
+# TODO: Depending on whether or not we also want to show album info
+# consider switching out commented code.
 def on_track_change(window, *args, **kwargs):
+    if window.ignore_properties_changed:
+        return
     print("Track changed:", args, kwargs)
 
-    # Check if 'Track' exists in the dictionary and if it itself
-    # is a dictionary
+    # Extract track information from arguments
     track_info = args[1].get('Track', {})
     print("Track info:", track_info)  # Debugging line
 
+    # Check if track information is available
     if isinstance(track_info, dict):
         title = track_info.get('Title', "Unknown")
-        album = track_info.get('Album', "Unknown")
+        # album = track_info.get('Album', "Unknown")
         artist = track_info.get('Artist', "Unknown")
 
         # Only emit the signal if at least one of the title, album,
         # or artist is known
-        if title != "Unknown" or album != "Unknown" or artist != "Unknown":
+        # if title != "Unknown" or album != "Unknown" or artist != "Unknown":
+        if title != "Unknown" or artist != "Unknown":
             # song_info = f"{title} - {album} Artist: {artist}"
             song_info = f"{title} - {artist}     "
             window.trackChanged.emit(song_info)
 
 
 class GLibThread(QThread):
-    # trackChanged = pyqtSignal(str)  # Define a new signal
-
     def run(self):
         loop = GLib.MainLoop()
         loop.run()
@@ -122,28 +181,112 @@ class GUI(QMainWindow):
 
         self.dbus_thread = GLibThread()
         self.trackChanged.connect(self.update_song_label)
-        try:
-            bus = SystemBus()
-
-            media_player = bus.get('org.bluez',
-                                   ('/org/bluez/hci0/dev_44_35_83_3E_0E_0A/'
-                                    'player0')
-                                   )  # Replace with your device's MAC
-
-            media_player.PropertiesChanged.connect(partial(
-                on_track_change, self))
-
-        except KeyError:
-            print("Failed to connect to the media player. "
-                  "Make sure the device is connected and try again.")
-
-        self.dbus_thread.start()
 
         self.scrolling_label = ScrollingLabel("Nothing playing")
         layout = QVBoxLayout()
         layout.addWidget(self.scrolling_label)
 
         self.scrollingLabelPlaceholder.setLayout(layout)
+
+        try:
+            self.bus = SystemBus()
+
+            # Set the flag to process the PropertiesChanged signal
+            self.ignore_properties_changed = False
+
+            self.bt_mac_address = get_connected_bluetooth_mac(self.bus, self)
+
+            if self.bt_mac_address:
+                print(f"Bluetooth device MAC address: {self.bt_mac_address}")
+                device_path = (f'/org/bluez/hci0/'
+                               f'{self.bt_mac_address.replace(":", "_")}'
+                               f'/player0')
+
+                print("dpath", device_path)
+
+                try:
+                    self.media_player = self.bus.get('org.bluez', device_path)
+                except Exception as e:
+                    print(f"Couldn't get media player: {e}")
+                    return
+
+                other_properties = self.media_player.GetAll('org.bluez'
+                                                            '.MediaPlayer1')
+                print("All properties:", other_properties)
+
+                # Get the current song information
+                current_track = self.media_player.Get('org.bluez.MediaPlayer1',
+                                                      'Track')
+                if current_track:
+                    print("Initial track:", current_track)
+                    self.update_label_with_track_info(current_track)
+
+                self.media_player.PropertiesChanged.connect(partial(
+                    on_track_change, self))
+
+            else:
+                print("No connected Bluetooth device found.")
+                self.scrolling_label.update_text("No media device connected")
+
+        except Exception as e:
+            print(f"An error occurred in GUI constructor: {e}")
+
+        self.dbus_thread.start()
+
+    def handle_new_connection(self):
+        try:
+            # Set the flag to process the PropertiesChanged signal
+            self.ignore_properties_changed = False
+
+            self.bt_mac_address = get_connected_bluetooth_mac(self.bus, self)
+            if self.bt_mac_address:
+                print(f"New Bluetooth device MAC address: "
+                      f"{self.bt_mac_address}")
+                time.sleep(2)  # Make sure the media player object is ready
+                device_path = (f'/org/bluez/hci0/'
+                               f'{self.bt_mac_address.replace(":", "_")}'
+                               f'/player0')
+
+                self.media_player = self.bus.get('org.bluez', device_path)
+
+                # Get the current song information
+                current_track = self.media_player.Get('org.bluez.MediaPlayer1',
+                                                      'Track')
+                if current_track:
+                    print("Initial track:", current_track)
+                    self.update_label_with_track_info(current_track)
+
+                self.media_player.PropertiesChanged.connect(partial(
+                    on_track_change, self))
+            else:
+                print("No connected Bluetooth device found.")
+                self.scrolling_label.update_text("No media device connected")
+        except Exception as e:
+            print(f"An error occurred in handle_new_connection(): {e}")
+
+    def handle_disconnection(self):
+        try:
+            # Set a flag to ignore the PropertiesChanged signal
+            self.ignore_properties_changed = True
+
+            # Reset UI elements
+            self.scrolling_label.update_text("No media device connected")
+
+            # Reset any internal state variables
+            self.bt_mac_address = None
+            self.media_player = None
+
+        except Exception as e:
+            print(f"An error occurred in handle_disconnection(): {e}")
+
+    def update_label_with_track_info(self, track_info):
+        title = track_info.get('Title', "Unknown")
+        album = track_info.get('Album', "Unknown")
+        artist = track_info.get('Artist', "Unknown")
+
+        if title != "Unknown" or album != "Unknown" or artist != "Unknown":
+            song_info = f"{title} - {artist}     "
+            self.trackChanged.emit(song_info)
 
     def update_song_label(self, new_song):
         self.scrolling_label.update_text(f"{new_song}")
